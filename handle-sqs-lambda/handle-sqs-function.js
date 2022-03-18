2e0b99f14154968cb87c7d3ff80d7d6f3fd9e34c
Tolga Yildirim <tolga.yildirim@gmail.com>
2022-03-18 10:56:51 -0400

    Add files via upload

--- handle-sqs-lambda/handle-sqs-function.js
+++ handle-sqs-lambda/handle-sqs-function.js
@@ -1,22 +1,28 @@
 var aws = require('aws-sdk')
-exports.handler = async function (event, context) {
+exports.handler = async function (event, context, callback) {
+    var stepfunctions = new aws.StepFunctions();
+    var params;
     event.Records.forEach(record => {
         const { body } = record;
         console.log(body);
-        var params = {
+        params = {
             stateMachineArn: process.env.QUEUE_ARN,
             input: JSON.stringify({ inputCaseID: body })
         };
-        var stepfunctions = new aws.StepFunctions();
-        console.log("Calling step functions...");
-        console.log(params)
-        stepfunctions.startExecution(params, (err, data) => {
-            if (err) {
-                console.log("Error while caling Step Func.", err);
-            } else {
-                console.log("Succesfully execurted Step Func.");
-            }
-        });
+       
+        // stepfunctions.startExecution(params, (err, data) => {
+        //     if (err) {
+        //         console.log("Error while caling Step Func.", err);
+        //         callback(err, JSON.stringify({ }))
+        //     } else {
+        //         console.log("Succesfully execurted Step Func.");
+        //         callback(null, JSON.stringify({ body }))
+        //     }
+        // });
     });
+    console.log(params)
+    console.log("Calling step functions...");
+    const success = await stepfunctions.startExecution(params).promise();
+    console.log(success)
     return {};
 }
